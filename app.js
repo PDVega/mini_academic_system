aca3c85a330df4bda2d1e8d9a05268dbac2fa3f1
pdvega <pdvegaa@gmail.com>
2017-07-18 19:43:22 +0700

    mini academic system ready to deploy to heroku

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+
+const index = require('./routes/index');
+const teachers = require('./routes/teachers')
+const subjects = require('./routes/subjects')
+const students = require('./routes/student')
+const user = require('./routes/register')
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(express.static(path.join(__dirname, 'public')))
+const session = require('express-session')
+
+app.use(session({
+  secret:'pdvega',
+  resave : false,
+  saveUninitialized : true
+  // cookie : { secure : true }
+}))
+
+app.use('/register', user);
+app.use('/', index)
+
+app.use((req, res, next) => {
+  if(req.session.user){
+    next();
+  }else{
+    res.render('login', {msg : 'Please login first'})
+  }
+})
+
+app.use('/teachers', teachers);
+app.use('/subjects', subjects);
+app.use('/students', students);
+
+
+app.listen(3000)
